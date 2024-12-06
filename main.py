3b540913fa293b75b173c824b64f6884357adf80
lookinALX <lookin.alx@gmail.com>
2024-12-06 18:28:47 +0100

    working version for rect and sq

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main():
     rectangle2 = shapes.Rectangle((255, 0, 255), 200, 200, 150, 100, 0.1, 0.1)
     #rectangle1 = shapes.Rectangle((0, 0, 255), 50, 50, 150, 100, 0.1, 0.1)
     #square1 = shapes.Square((255, 0, 0), 500, 300, 200, 0, 0)
-    #square2 = shapes.Square((255, 255, 0), 100, 100, 20, 0.1, 0.05)
+    square2 = shapes.Square((255, 255, 0), 100, 100, 20, 0.1, 0.05)
     circle1 = shapes.Circle(color=(255, 0, 0), x=100, y=100, radius=20, speed_x=0.1, speed_y=0.1)
     # Бесконечный цикл программы
     running = True
@@ -26,13 +26,13 @@ def main():
         rectangle2.draw(screen)
         #rectangle1.draw(screen)
         #square1.draw(screen)
-        #square2.draw(screen)
+        square2.draw(screen)
         circle1.draw(screen)
         # Move objects
         #rectangle1.move()
         #rectangle1.check_collision(rectangle2)
-        #square2.check_collision(square1)
-        #square2.move()
+        square2.check_collision(rectangle2)
+        square2.move()
         circle1.move()
         circle1.check_collision(rectangle2)
 
--- shapes.py
+++ shapes.py
@@ -102,19 +102,14 @@ class Rectangle(Shape):
         pygame.transform.rotate(surface, -angle)
 
     def check_collision(self, other_rect):
-      collision_detected = False
-      if (self.x + self.width >= other_rect.x and self.y + self.height >= other_rect.y) and (
-            other_rect.y + other_rect.height >= self.y and other_rect.x + other_rect.width >= self.x):
-        collision_detected = True
-        print("COLLISION!")
-      else:
         collision_detected = False
-      return collision_detected
-
-
-
-
-
+        if (self.x + self.width >= other_rect.x and self.y + self.height >= other_rect.y) and (
+                other_rect.y + other_rect.height >= self.y and other_rect.x + other_rect.width >= self.x):
+            collision_detected = True
+            print("COLLISION!")
+        else:
+            collision_detected = False
+        return collision_detected
 
 
 class Square(Rectangle):
@@ -144,42 +139,12 @@ class Square(Rectangle):
     def __init__(self, color, x, y, size, speed_x=0, speed_y=0):
         super().__init__(color, x, y, size, size, speed_x, speed_y)
         self.size = size
-        self.size = size
+        self.width = size
+        self.height = size
 
     def draw(self, surface):
         pygame.draw.rect(surface, self.color, (self.x, self.y, self.size, self.size))
 
-    def move(self):
-        self.x += self.speed_x
-        self.y += self.speed_y
-
-        if self.x + self.size > constants.SCREEN_WIDTH or self.x < 0:
-            self.speed_x = -self.speed_x
-
-        if self.y + self.size > constants.SCREEN_HEIGHT or self.y < 0:
-            self.speed_y = -self.speed_y
-
-    def rotate(self, surface, angle):
-        pygame.transform.rotate(surface, -angle)
-
-    def check_collision(self, other_rect):
-        """
-        Проверка коллизии с другим объектом.
-
-        Parameters:
-            other_rect (class Square): Прямоугольник другого объекта
-
-        Returns:
-            bool: True, если объекты пересекаются, иначе False
-        """
-        collision_detected = False
-        if (self.x + self.size >= other_rect.x and self.y + self.size >= other_rect.y) and (
-                other_rect.y + other_rect.size >= self.y and other_rect.x + other_rect.size >= self.x):
-            collision_detected = True
-            print("COLLISION!")
-        else:
-            collision_detected = False
-        return collision_detected
 
 class Circle(Shape):
     """
@@ -219,14 +184,14 @@ class Circle(Shape):
         self.y += self.speed_y
         self.center = [self.x, self.y]
 
-        if(self.x- self.radius <= 0 or self.x + self.radius >= constants.SCREEN_WIDTH ):
+        if self.x - self.radius <= 0 or self.x + self.radius >= constants.SCREEN_WIDTH:
             self.speed_x = -self.speed_x
 
-        if (self.y - self.radius <= 0 or self.y + self.radius >= constants.SCREEN_HEIGHT):
+        if self.y - self.radius <= 0 or self.y + self.radius >= constants.SCREEN_HEIGHT:
             self.speed_y = -self.speed_y
-
-
-
 
     def rotate(self, surface, angle):
         pygame.transform.rotate(surface, -angle)
+
+    def check_collision(self, other_rect):
+        pass

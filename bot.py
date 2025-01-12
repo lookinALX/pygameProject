0bd6c43a1cc2ad7c6dbc8cb5f6f832f08c0c7b05
lookinALX <lookin.alx@gmail.com>
2025-01-12 19:00:54 +0100

    some todos

--- bot.py
+++ bot.py
@@ -1,123 +1,19 @@
 import pygame
-import random
-import math
 import constants
-
-pygame.init()
-
+import shapes
 
 
-
-
-
-
-
-
-
-class bot():
+class Bot(shapes.Rectangle):
     def __init__(self, color: tuple, x: float, y: float, width: float, height: float, speed_x=0, speed_y=0):
-        super().__init__(x, y, speed_x, speed_y, color)
-        self.width = width
-        self.height = height
-        self.dragging = False
+        super().__init__(color, x, y, width, height, speed_x, speed_y)
 
     def draw(self, surface):
         pygame.draw.rect(surface, self.color, (self.x, self.y, self.width, self.height))
 
-    def move(self , other_rect):
-        self.x += self.speed_x
-        self.y += self.speed_y
+    #TODO: Napisat'
+    def move(self, ball):
+        pass
 
-
-        if(other_rect.x <= 400):
-        self.y = other_rect.y
-
-
-
-
-
-
-
-
-
-
-
-        if self.x <= 0 or self.x + self.width >= constants.SCREEN_WIDTH:
-            self.speed_x = -self.speed_x
-
-        if self.y <= 0 or self.y + self.height >= constants.SCREEN_HEIGHT:
-            self.speed_y = -self.speed_y
-
-    def rotate(self, surface, angle):
-        pygame.transform.rotate(surface, -angle)
-
+    # TODO: Perepisat'
     def check_collision(self, other_rect):
-        collision_detected = False
-        if (self.x + self.width >= other_rect.x and self.y + self.height >= other_rect.y) and (
-                other_rect.y + other_rect.height >= self.y and other_rect.x + other_rect.width >= self.x):
-            collision_detected = True
-            print("COLLISION!")
-        else:
-            collision_detected = False
-        return collision_detected
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        pass
--- main.py
+++ main.py
@@ -12,10 +12,6 @@ def main():
     gates1 = shapes.Rectangle((0, 128, 0), 0, 0, 10, 600,0,0  )
     gates2 = shapes.Rectangle((0, 128, 0), 790, 0, 10, 600, 0, 0)
     middle = shapes.Rectangle((0, 0, 255), 400, 0, 5, 600, 0, 0)
-    bot = shapes.bot((255,0,255),0,0,50,150,0,0.6)
-    # rectangle1 = shapes.Rectangle((0, 0, 255), 50, 50, 150, 100, 0.1, 0.1)
-    # square1 = shapes.Square((255, 0, 0), 500, 300, 200, 0, 0)
-    # square2 = shapes.Square((255, 255, 0), 100, 100, 20, 0.1, 0.05)
     circle1 = shapes.Circle(color=(255, 0, 0), x=400, y=350, radius=20, speed_x=0.3, speed_y=0.3)
     # Бесконечный цикл программы
     running = True
@@ -36,23 +32,12 @@ def main():
         gates1.draw(screen)
         gates2.draw(screen)
 
-
         middle.draw(screen)
 
         rectangle2.drag()
-        # rectangle1.draw(screen)
-        # square1.draw(screen)
-        # square2.draw(screen)
         circle1.draw(screen)
         # Move objects
-        # rectangle1.move()
-        # rectangle1.check_collision(rectangle2)
-        # square2.check_collision(rectangle2)
-        # square2.move()
         circle1.move()
-        #rectangle2.move()
-        #circle1.check_collision(rectangle2)
-        #circle1.change_direction()
         circle1.change_direction_if_collision(rectangle2)
 
         pygame.display.update()

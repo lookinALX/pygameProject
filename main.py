8d09f1982df866ef63989cd01eaa5f606324083f
lookinALX <lookin.alx@gmail.com>
2025-01-24 18:01:53 +0100

    some changes

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main():
     gates1 = shapes.Rectangle((0, 128, 0), 0, 0, 10, 600,0,0  )
     gates2 = shapes.Rectangle((0, 128, 0), 790, 0, 10, 600, 0, 0)
     middle = shapes.Rectangle((0, 0, 255), 400, 0, 5, 600, 0, 0)
-    circle1 = shapes.Circle(color=(255, 0, 0), x=400, y=350, radius=20, speed_x=0.1, speed_y=0.1)
+    circle1 = shapes.Circle(color=(255, 0, 0), x=400, y=21, radius=20, speed_x=0.1, speed_y=0.1)
     bot_rocket = bot.Bot((100, 50, 155), 90, 200, 50, 150, 0, 0.08)
     # Бесконечный цикл программы
     running = True
@@ -30,7 +30,7 @@ def main():
 
         screen.fill((255, 255, 255))
         """Draw objects"""
-        bot_rocket.draw(screen)
+        #bot_rocket.draw(screen)
         rectangle2.draw(screen)
         gates1.draw(screen)
         gates2.draw(screen)
@@ -41,7 +41,7 @@ def main():
         # Move objects
         circle1.move()
         circle1.change_direction_if_collision(rectangle2)
-        circle1.change_direction_if_collision(bot_rocket)
+        # circle1.change_direction_if_collision(bot_rocket)
         bot_rocket.follow_ball(circle1)
 
         pygame.display.update()
--- shapes.py
+++ shapes.py
@@ -121,6 +121,7 @@ class Rectangle(Shape):
                 print("COLLISION!")
         return collision_detected
 
+    #TODO: Подумать если объект столкнулся с мячом то нельзя двигать объект в мяч
     def drag(self):
         if self.dragging:
             delta_pos = pygame.mouse.get_rel()
@@ -225,13 +226,7 @@ class Circle(Shape):
     def change_direction_if_collision(self, other_rect):
         collision_detected = self.check_collision(other_rect)
         if collision_detected:
-            is_x = (other_rect.y <= self.y <= other_rect.y + other_rect.height)
-            is_y = (other_rect.x <= self.x <= other_rect.x + other_rect.width)
-            collision_up = is_y and self.y + self.radius >= other_rect.y
-            collision_down = is_y and self.y - self.radius <= other_rect.y + other_rect.height
-            collision_left = is_x and self.x + self.radius >= other_rect.x
-            collision_right = is_x and self.x - self.radius <= other_rect.x + other_rect.width
-            if collision_right or collision_left:
+            if other_rect.y <= self.y <= other_rect.y + other_rect.height:
                 self.speed_x = -self.speed_x
-            if collision_down or collision_up:
+            else:
                 self.speed_y = -self.speed_y

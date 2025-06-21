48a6ceaa3e6ec33c728bba95eaf5a117c1da857e
lookinALX <lookin.alx@gmail.com>
2025-06-21 19:30:47 +0200

    new todo for bot

--- bot.py
+++ bot.py
@@ -11,20 +11,22 @@ class Bot(shapes.Rectangle):
         pygame.draw.rect(surface, self.color, (self.x, self.y, self.width, self.height))
 
 
-    #TODO: понять что тут происходит!
     def follow_ball(self, ball: shapes.Circle):
-        a = 0
+        y_where_ball_comes = 0
         half_field = constants.SCREEN_WIDTH / 2
         is_crossed_half_field = True if ball.x >= half_field else False
 
+        #TODO: Посчитать правильно y_where_ball_comes через время и скорости
         if not is_crossed_half_field:
-            d = ball.x - self.x - self.width
-            ctan_phi = ball.speed_y / ball.speed_x
-            a = d * ctan_phi + ball.y
+            distance_between_ball_and_bot = ball.x - self.x - self.width
+            y_where_ball_comes = ???
 
-        if (self.y + self.width / 2 <= a) and (not is_crossed_half_field):
+        if (self.y + self.height / 2 < y_where_ball_comes) and (not is_crossed_half_field):
             self.y += self.speed_y
-        elif (self.y + self.width / 2 >= a) and (not is_crossed_half_field):
+            print(f"bot y --> {self.y} ; y where ball comes --> {y_where_ball_comes}")
+        elif (self.y + self.height / 2 > y_where_ball_comes) and (not is_crossed_half_field):
             self.y -= self.speed_y
+            print(f"bot y --> {self.y} ; y where ball comes --> {y_where_ball_comes}")
         else:
             self.y = self.y
+            print(f"bot y --> {self.y} ; y where ball comes --> {y_where_ball_comes}")
